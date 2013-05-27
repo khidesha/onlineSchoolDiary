6506dc1dc854733986e5177bcded4c0001310e15
khidesha <khidesha@khidesha-PC>
2013-05-27 16:29:33 +0400

    Class.java

--- onlineSchoolDiary/src/NonUseClasses/Class.java
+++ onlineSchoolDiary/src/NonUseClasses/Class.java
@@ -1,5 +1,20 @@
 package NonUseClasses;
 
 public class Class {
+	private int classId;
+	private String className;
 
+	public Class(int class_id, String className) {
+		this.classId = class_id;
+		this.className = className;
+	}
+	
+	int getClassId(){
+		return classId;
+	}
+	
+	
+	String getClassName(){
+		return className;
+	}
 }
