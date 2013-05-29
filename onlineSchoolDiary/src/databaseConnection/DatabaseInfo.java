a5fb5c0bf09bceb3761b44239cf1640feb486e9a
vamekhgoiati <vgoia10@freeuni.edu.ge>
2013-05-29 22:30:22 +0400

    check

---
+++ onlineSchoolDiary/src/databaseConnection/DatabaseInfo.java
@@ -0,0 +1,15 @@
+package databaseConnection;
+
+import java.sql.Connection;
+
+
+public class DatabaseInfo {
+	
+	private Connection con;
+	
+	public DatabaseInfo(){
+		con = DBConnection.getConnection();
+	}
+	
+	
+}
