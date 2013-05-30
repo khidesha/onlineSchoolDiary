4d76036f8c042130f09e15ae625fcb563a4e0f86
ZUCA <ZUCA@Home>
2013-05-30 16:37:04 +0400

    ganaxlebuli useri

--- onlineSchoolDiary/src/classes/User.java
+++ onlineSchoolDiary/src/classes/User.java
@@ -4,11 +4,15 @@ public class User {
 	public int user_id;
 	public String user_name;
 	public int school_id;
+	public String user_status;
+	public String password;
 	
-	public User(int userId, String userName, int schoolId) {
+	public User(int userId, String userName, int schoolId, String pass, String userStatus) {
 		this.user_id = userId;
 		this.user_name = userName;
 		this.school_id = schoolId;
+		this.user_status = userStatus;
+		this.password = pass;
 	}
 	
 	public int getUserId() {
@@ -21,6 +25,34 @@ public class User {
 	
 	public int getSchoolId() {
 		return school_id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getUserStatus() {
+		return user_status;
+	}
+	
+	public void setPassword(String pass) {
+		this.password = pass;
+	}
+	
+	public void setUserStatus(String status) {
+		this.user_status = status;
+	}
+	
+	public void setUserId(int userId) {
+		this.user_id = userId;
+	}
+	
+	public void setUserName(String userName) {
+		this.user_name = userName;
+	}
+	
+	public void setSchoolId(int schoolId) {
+		this.school_id = schoolId;
 	}
 	
 	
