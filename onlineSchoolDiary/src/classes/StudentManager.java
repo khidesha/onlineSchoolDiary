758de1758ae5356539aa046975896e9cc3d397d0
ZUCA <ZUCA@Home>
2013-06-08 15:57:11 +0400

    bodishit gamomrcha :(

---
+++ onlineSchoolDiary/src/classes/StudentManager.java
@@ -0,0 +1,64 @@
+package classes;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class StudentManager {
+	static Statement statement;
+
+	public StudentManager(Statement statement) {
+		this.statement = statement;
+	}
+
+
+	public static ArrayList<Subject> getSubjectes(int classId) {
+		try {
+			ArrayList<Subject> arr = new ArrayList<Subject>();
+			int subjectId;
+			int teacherId;
+			String subjectName;
+			Subject subject;
+			ResultSet set = statement.executeQuery("Select * from subjects where class_id=" + classId);
+			while (set.next()) {
+				subjectId = set.getInt("subject_id");
+				subjectName = set.getString("subject_name");
+				teacherId = set.getInt("teacher_id");
+				subject = new Subject(subjectId, subjectName, teacherId, classId);
+				arr.add(subject);
+			}
+			return arr;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static ArrayList<Subject> getMarks(int classId) {
+		try {
+			ArrayList<Subject> arr = new ArrayList<Subject>();
+			int subjectId;
+			int teacherId;
+			String subjectName;
+			Subject subject;
+			ResultSet set = statement.executeQuery("Select * from subjects where class_id=" + classId);
+			while (set.next()) {
+				subjectId = set.getInt("subject_id");
+				subjectName = set.getString("subject_name");
+				teacherId = set.getInt("teacher_id");
+				subject = new Subject(subjectId, subjectName, teacherId, classId);
+				arr.add(subject);
+			}
+			return arr;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
